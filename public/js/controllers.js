89989468674b97448a51d8333d55dc6e18b31ee5
march-dave <ysangy@gmail.com>
2016-06-04 13:49:09 -0700

    clean logs

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,25 +3,15 @@
 var app = angular.module('routerApp');
 
 app.controller('homeCtrl', function($q, $http) {
-  console.log('homeCtrl');
+
 });
 
 app.controller('listCtrl', function($scope, $state, pokedex) {
-  console.log('listCtrl');
-  console.log('pokedex:', pokedex);
-
   $scope.pokedex = pokedex;
 });
 
-
-
 app.controller('detailCtrl', function($scope, $interval, pokemon) {
-  console.log('detailCtrl');
-  console.log('pokemon:', pokemon);
   $scope.pokemon = pokemon;
-
-  console.log('sprites:', $scope.pokemon.sprites);
-  
   var sprites = pokemon.sprites;
   var keys = Object.keys(sprites);
   var numSprites = keys.length;
@@ -37,11 +27,3 @@ app.controller('detailCtrl', function($scope, $interval, pokemon) {
   }, 1000);
 
 })
-
-
-
-
-
-
-
-
